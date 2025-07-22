02b19335a0aef1b9e9dbead8d9b1cf447c3b7b76
S-Hellier <stopphellier@gmail.com>
2025-07-21 22:12:08 -0400

    directing bot route to app

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,10 +1,10 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
-from bot import messageFlow
+from app.bot import messageFlow
 
 scheduler = AsyncIOScheduler()
 
 def startScheduler():
-    trigger = CronTrigger(day_of_week='mon-thu', hour=22, minute=0)
+    trigger = CronTrigger(day_of_week='mon-thu', hour=22, minute=20)
     scheduler.add_job(messageFlow, trigger)
     scheduler.start()
